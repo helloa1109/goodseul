3d40ed3c7f92c9270aa64cdc0f2e7081634b3c41
HYUNG MIN KIM <freshtuna88@hotmail.com>
2023-09-05 19:39:27 +0900

    형민 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          이상혁 페이커
         </p>
         <a
           className="App-link"
