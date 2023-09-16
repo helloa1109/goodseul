a1947d1ddc9586541dcff7d14ba6c750f7e254bf
helloa1109 <helloa1109@naver.com>
2023-09-16 16:45:54 +0900

    리코일 수정

--- src/recoil/header/HeaderAtom.ts
+++ src/recoil/header/HeaderAtom.ts
@@ -1,10 +1,5 @@
 import { atom } from 'recoil';
 
-export const currentPageState = atom({
-  key: 'currentPage',
-  default: '/',
-});
-
 export const HeaderMenuModal = atom ({
     key : 'HeaderMenuModal',
     default : false,
