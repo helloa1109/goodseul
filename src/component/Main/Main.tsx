0a50809501be4a6e7a7c773c7ca2f832acaafcc7
Alpac495 <107226944+Alpac495@users.noreply.github.com>
2023-09-20 19:27:23 +0900

    main fix/file add

--- src/component/Main/Main.tsx
+++ src/component/Main/Main.tsx
@@ -3,7 +3,26 @@ import React from 'react'
 const Main = () => {
   return (
     <div>
-     
+      <div className='headermain'></div>
+      <div className='main_remotewrap'>
+        <div className='main_slogan'></div>
+        <div className='main_remote'></div>
+      </div>
+      <div className='main_onair'>
+        <div className='main_onair'></div>
+      </div>
+      <div className='main_review'>
+        <div className='main_review'></div>
+      </div>
+      <div className='main_locatewrap'>
+        <div className='main_locate'></div>
+      </div>
+      <div className='main_purposewrap'>
+        <div className='main_purpose'></div>
+      </div>
+      <div className='main_estimatewrap'>
+        <div className='main_estimate'></div>
+      </div>
     </div>
   )
 }
