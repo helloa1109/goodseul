5be9373a09acf5ad9f0b9008d016db0fe5529de7
helloa1109 <helloa1109@naver.com>
2023-09-20 15:47:03 +0900

    헤더 디자인 및 위치기반 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ import Test1 from './component/Main/Test1';
 import {PlayMain,PlayRouter} from './pages/plays';
 import Login from './component/Login/Login';
 import Join from './component/Join/Join';
-
+import Category from './component/Category/Category';
+import Review from "./component/Review/Review";
+import LocationBased from './component/LocationBased/LocationBased';
 
 
 function App() {
@@ -21,11 +23,15 @@ function App() {
         <Route path='/test' element={<Test1 />} />
         <Route path='/play' element={<PlayMain />} />
         <Route path='/play/:pID' element={<PlayRouter />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/join' element={<Join/>}/>
+        <Route path='/Review' element={<Review/>}/>
+        <Route path='/Category' element={<Category/>}/>
+        <Route path='/Location' element={<LocationBased/>}/>
+
         <Route path="/*" element={
           <h1 style={{ width: "100%", textAlign: "center", marginTop: "10%", position: "absolute" }}>페이지가 없습니다</h1>
         } />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/join' element={<Join/>}/>
       </Routes>
     </BrowserRouter>
   );
