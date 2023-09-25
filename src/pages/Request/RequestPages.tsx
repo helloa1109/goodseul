fc5fe448718782a9c33b4f53165a6bd8716d33dd
helloa1109 <helloa1109@naver.com>
2023-09-25 14:02:03 +0900

    견적요청페이지, 위치기반페이지

---
+++ src/pages/Request/RequestPages.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Request from '../../component/Request/Request'
+
+const RequestPages = () => {
+  return (
+    <div>
+      <Request/>
+    </div>
+  )
+}
+
+export default RequestPages
