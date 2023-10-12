9f95c2fb616dbc894cd9a00682656580b99713ac
cyj <itnaupna@gmail.com>
2023-10-12 15:08:11 +0900

    플레이2 1차 완성

--- src/component/plays/Playlist.tsx
+++ src/component/plays/Playlist.tsx
@@ -13,8 +13,8 @@ const Playlist = () => {
             str='출석체크.' str2='여기 설명 한줄 추가' order={1}/>
             <PlayBanner dest='ball' src="http://dopeboyzclub.ddns.net:7733/testimg/wow.png"
             str='플레이1.' str2='애오오옹' order={0}/>
-            <PlayBanner dest='null' src="http://dopeboyzclub.ddns.net:7733/testimg/dailyimg.png"
-            str='구슬샵.' str2='여기 설명 한줄 추가' order={1}/>
+            <PlayBanner dest='run' src="http://dopeboyzclub.ddns.net:7733/testimg/play_run.png"
+            str='플레이2.' str2='여기 설명 한줄 추가' order={1}/>
         </ul>
     );
 };
