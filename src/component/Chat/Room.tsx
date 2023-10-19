c1d6e25fa5b26a5d6e407d7c41dbc3964f1ec3ec
helloa1109 <helloa1109@naver.com>
2023-10-19 19:53:35 +0900

    채팅 api 수정

--- src/component/Chat/Room.tsx
+++ src/component/Chat/Room.tsx
@@ -6,8 +6,12 @@ import { person1State, person2State } from "../../recoil/Chat/ChatAtom";
 import "../../style/Chat/Chat.scss";
 import { useParams } from "react-router-dom";
 import { Message } from "../../hooks/Chat/ChatType";
+import { JWTDecoding } from "../../apis/JWT/JWTDecoding";
+import { decodeToken } from "../../hooks/JWT/JWTType";
+import emoji from "../../image/Chat/emoji.png";
+import chatprofile from "../../image/Chat/profile.png";
 
-
+import { useLocation } from "react-router-dom";
 
 const Room = () => {
 
@@ -15,64 +19,61 @@ const Room = () => {
     const person1 = useRecoilValue(person1State);
     const person2 = useRecoilValue(person2State);
 
+    const sock = new SockJS("http://dopeboyzclub.ddns.net:7780/ws");
+
+
     const { roomId } = useParams();
-    
+
+    // const ttValue = useRecoilValue(ttAtom);
+
+    // console.log("디코딩된 idx",ttValue);
+    const ws = StompJS.Stomp.over(sock);
     const [chatClient, setChatClient] = useState<StompJS.CompatClient | null>(null);
 
     const msgRef = useRef<HTMLInputElement>(null);
     const [msg, setMsg] = useState<Message[]>([]);
 
-    // sender에는 현재로그인한 유저의 jwt 디코딩 후 나온 idx 값을 넣음
+    // sender에는 현재로그인한 유저의 jwt 디코딩 후 나온 idx 값을 넣음 ?
     // receiver에는 (현재는 임시인 상황)
     // 기업회원의 idx를 받아와서 기업 <-> 유저간 실시간 채팅이 되도럭 값을 넣음
-    const sender = person1;
-    const receiver = person2;
 
+    // const sender = person1;
+    // const receiver = person2;
+    const location = useLocation();
+    const senderNow = ((JWTDecoding() as decodeToken).idx);
+    const sendGetData = location.state;
+    // console.log("now",senderNow ,sendGetData.sender);
     // 채팅 스크롤 관련 ref
-    const chatRef = useRef<null | HTMLDivElement>(null); 
+    const chatRef = useRef<null | HTMLDivElement>(null);
 
     console.log(person1);
-    console.log("sender", sender);
-    console.log("receiver", receiver);
+    // console.log("sender", sendGetData.sender);
+    // console.log("receiver", sendGetData.receiver);
     console.log(roomId);
 
     useEffect(() => {
-        // if(chatClient){
-            
-            connect();
-        // }
-        // return () => disConnect();
+
+        setChatClient(ws);
+        setMsg([]);
+        connect();
+        return () => disConnect();
+        
     }, []);
+    console.log("dd",chatClient);
+    const disConnect = () => {
 
-    // const disConnect = () => {
-
-    //     if (chatClient === null) {
-    //         return;
-    //     }
-    //     chatClient?.deactivate();
-    // };
-
-
-    // const handleconnect = () => {
-    //     connect(
-    //         () => {
-    //             setChatClient(ws);
-    //             ws.subscribe(`/sub/${roomId}`, data => {
-    //                 AddChat(data);
-    //             });
-    //             enter(sender, receiver, "rr");
-    //         },
-    //         (error:any) => {
-    //             console.log("WebSocket 연결 에러:", error);
-    //         }
-    //     );
-    // };
+        if (chatClient === null) {
+            return;
+        }
+        chatClient.deactivate();
+    };
 
     const connect = () => {
-        const sock = new SockJS("http://dopeboyzclub.ddns.net:7780/ws");
-        const ws = StompJS.Stomp.over(sock);
-        setChatClient(ws);
+        disConnect();
 
+        setChatClient(ws);
+        // ws.disconnect();
+        
         ws.debug = function (str) {
             console.log(str);
         };
@@ -84,10 +85,17 @@ const Room = () => {
                 console.log(JSON.parse(data.body));
                 AddChat(data);
             });
-            enter(sender, receiver, "입장ㄱㄱ");
+            enter(sendGetData.sender, sendGetData.receiver, "입장");
         }, (error: any) => {
             console.log("error", error);
-        })
+        });
+
+        // sock.onclose = () => {
+        //     console.log("닫힘");
+        //     setTimeout(() => {
+        //         connect();
+        //     }, 1000);
+        // };
     };
 
     const AddChat = (data: { body: string }) => {
@@ -106,14 +114,14 @@ const Room = () => {
                 receiver,
                 message: message.current?.value,
             }));
+            if (message.current) {
+                message.current.value = '';
+            }
         } else {
             console.log("클라이언트 null");
         }
+    }
 
-        if (message.current) {
-            message.current.value = '';
-        }
-    };
 
     const enter = (sender: number, receiver: number, message: string) => {
         if (chatClient) {
@@ -130,7 +138,7 @@ const Room = () => {
 
     const handleOneKeyEnter = (e: any) => {
         if (chatClient) {
-            publish(sender, receiver, msgRef);
+            publish(sendGetData.sender, sendGetData.receiver, msgRef);
         } else {
             console.log("클라이언트 Null");
         }
@@ -143,9 +151,19 @@ const Room = () => {
         }
     }
 
+    // 자동으로 맨 아래있는 텍스트로 이동
     const scrollToBottom = () => {
-            chatRef.current?.scrollIntoView({ behavior: 'smooth',block : 'end' });
+        chatRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
     };
+
+    const newDate = new Date();
+    const formatDate = (date: any) => {
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        return `${year}년 ${month}월 ${day}일`;
+    };
+    console.log(newDate);
 
     useEffect(() => {
         scrollToBottom();
@@ -153,24 +171,45 @@ const Room = () => {
 
     return (
         <div className="ChatRoom">
-            <div>roomID : {roomId ? roomId : null}</div>
-            <div className="chats"  ref={chatRef}>
+            {/* <div>roomID : {roomId ? roomId : null}</div> */}
+            <div className="chats" ref={chatRef}>
+                <div className="ChatDateGroup">
+                    <div className="Chatdate">{formatDate(newDate)}</div>
+                </div>
                 {msg.map((item, index) => (
                     <div className="Chattest">
                         <div key={index} className="chatMyInfo">
-                            <span className="ChatMyText">{item.sender} : {item.message} </span>
-                        </div>
-                        <div className="GoodSeulInfo">
-                            <span className="GoodSeulText">{item.receiver} ㅎㅇ</span>
+                            <div className="GoodSeulInfo">
+                                {
+                                    senderNow === item.sender ? (
+                                        <span className="ChatMyText"><span className="ChatMyTextSpan">{item.message}</span> </span>
+                                    ) :
+                                        <div className="ChatOtherTextGroup">
+                                            <div className="ChatOtherSection">
+                                                <img src={chatprofile} className="ChatProfilePng" alt="ChatProfile" />
+                                            </div>
+                                            <div className="ChatOtherInfoGroup">
+                                                <div className="ChatOtherSenderGroup">
+                                                    <span className="GoodSeulSender">{item.sender} </span>
+                                                </div>
+                                                <div className="GoodSeulText">
+                                                    <span className="GoodSeulTextSpan"> {item.message}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                }
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>
 
             <div className="toolbox">
-                <input placeholder="보낼 메시지" ref={msgRef} style={{ width: '80%' }} type="text" className="chatinput"
-                    onKeyDown={handleOneKeyPress}></input>
-                <button style={{ width: '17%' }} className="chatBtn"
+                <input placeholder="보낼 메시지" ref={msgRef} type="text" className="chatinput"
+                    onKeyDown={handleOneKeyPress}>
+                </input>
+                <img src={emoji} className="ChatEmoji" alt="ChatEmoji" />
+                <button className="chatBtn"
                     onClick={handleOneKeyEnter}>전송</button>
             </div>
         </div>
