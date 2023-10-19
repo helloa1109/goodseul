1d1e8c10f03c3eba276f69545a72bc72860cec5e
cyj <itnaupna@gmail.com>
2023-10-19 10:56:14 +0900

    FIX : JWTDecoding() 함수 수정
    해당함수 호출때 로그인정보 만료 후 로그인시 원래 페이지로 돌아가는 기능 추가

--- src/component/plays/PlayBanner.tsx
+++ src/component/plays/PlayBanner.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import FontRed from './FontRed';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { isLoginState } from '../../recoil/JWT/JWTAtom';
 type csProps = {
-    dest:string,
+    dest: string,
     src: string,
     str: string,
     order: number,
     str2?: string,
 }
-const PlayBanner = ({dest, src, str, str2, order }: csProps) => {
+const PlayBanner = ({ dest, src, str, str2, order }: csProps) => {
+    const isLogin = useRecoilValue(isLoginState);
     const navi = useNavigate();
-    const handleClick = (dest:string) => {
-        navi(`./${dest}`);
+    
+    const handleClick = (dest: string) => {
+        if (isLogin)
+            navi(`./${dest}`);
+        else {
+            alert('로그인해야 이용 가능합니다.');
+            navi(`/Login?returnPath=${dest}`)
+        }
     }
 
     return (
-        <li onClick={()=>{handleClick(dest)}}style={{
+        <li onClick={() => { handleClick(dest) }} style={{
             backgroundImage:
                 order % 2 === 0
                     ? `linear-gradient(75deg,rgba(255,255,255,1) 25%,rgba(255,255,255,0) 65%),url(${src})`
